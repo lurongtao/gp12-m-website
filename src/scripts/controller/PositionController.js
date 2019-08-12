760dd181ca81a6c37638c11d30c0a8c9cbb1eedb
lurongtao <lurongtao@126.com>
2019-08-12 18:05:22 +0800

    2019-08-12

--- src/scripts/controller/PositionController.js
+++ src/scripts/controller/PositionController.js
@@ -9,6 +9,8 @@ class PositionController {
     // this.render()
   }
   async render() {
+    let pageNo = 2
+
     $('main').html(positionTpl)
 
     let list = (await Http.get()).content.data.page.result
@@ -17,29 +19,86 @@ class PositionController {
       list
     })
 
-    $('#pos-list').html(html)
+    $('#pos-list ul li.head').after(html)
 
     let bScroll = new BScroll('#pos-list', {
-      probeType: 2
+      probeType: 2,
+      pullDownRefresh: true
     })
 
     // 初始化scroll位置，向上滚动 40px
     bScroll.scrollTo(0, -40)
 
     // 下拉刷新
-    // bScroll.on('scroll', function() {
-    //   if (this.y < 20 && this.y > -40) {
+    let $head = $('#pos-list .head img')
+    let $foot = $('#pos-list .foot img')
 
-    //   } else {
-    //     console.log(0)
-    //   }
-    // })
+    bScroll.on('scroll', function() {
+      if (this.y > 20 || this.y < -40) {
+        $head.addClass('up')
+      }
+
+      let detaY = this.y - this.maxScrollY
+      if (detaY < 0 ) {
+        $foot.addClass('down')
+      }
+
+    })
 
     bScroll.on('touchEnd', function() {
       if (this.y < 20 && this.y > -40) {
         this.scrollTo(0, -40, 200)
-      } else {
-        console.log(0)
+      }
+      if (this.y > 20) {
+        $head.attr('src', '/assets/images/ajax-loader.gif')
+
+        $.ajax({
+          url: '/api/listmore.json?pageNo=2&pageSize=2',
+          success: (result) => {
+            
+            let data = result.content.data.page.result
+            list = [ ...data, ...list ]
+            let html = template.render(positionListTpl, {
+              list
+            })
+            $('#pos-list ul li.head').after(html)
+
+            this.refresh()
+            this.scrollTo(0, -40, 200)
+            $head.removeClass('up')
+            $head.attr('src', '/assets/images/arrow.png')
+          }
+        })
+      }
+
+      let detaY = this.y - this.maxScrollY
+
+      if (detaY > 0 && detaY < 40 ) {
+        this.scrollTo(0, this.maxScrollY + 40, 200)
+      }
+
+      if (detaY < 0) {
+        $foot.attr('src', '/assets/images/ajax-loader.gif')
+
+        $.ajax({
+          url: `/api/listmore.json?pageNo=${pageNo}&pageSize=15`,
+          success: (result) => {
+            
+            let data = result.content.data.page.result
+            list = [ ...list, ...data ]
+            let html = template.render(positionListTpl, {
+              list
+            })
+            $('#pos-list ul li.foot').before(html)
+
+            this.refresh()
+            this.scrollTo(0, this.maxScrollY + 40, 200)
+            $foot.removeClass('down')
+            $foot.attr('src', '/assets/images/arrow.png')
+
+            pageNo++
+          }
+        })
       }
     })
 
